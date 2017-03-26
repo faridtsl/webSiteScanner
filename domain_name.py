4e54f5629e4e2b1a3f06a7ef767ab5130eb1cd85
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-26 04:28:58 +0100

    Added tld library files to project so that user won't have to install it

--- domain_name.py
+++ domain_name.py
@@ -1,6 +1,5 @@
-import re
+import tld
 
 def get_domain_name(url):
     print('Top level domain ...')
-    tld = re.sub(r'((http|https):\/\/)?www(\.[^\.]*)*(\.[^\/]{4,}\.[^\/]*).*', r'\4', url, re.IGNORECASE)
-    return tld[1:]
+    return tld.get_tld(url)
