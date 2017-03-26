145e706ade7ae309380a76a286a1bb2473db83c8
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-26 00:11:41 +0000

    Merging some code lines

--- whois.py
+++ whois.py
@@ -5,7 +5,5 @@ def get_whois(url):
     print('Whois ...')
     command = 'whois ' + url
     process = os.popen(command)
-    result = ''
-    for line in process:
-        result = result + line
-    return result
+    return str(process.read())
+
